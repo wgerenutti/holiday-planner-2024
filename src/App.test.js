98838d79dd9d0f527b22a2c7c0ee600b89f03df0
WIlliam Gerenutti <williamgerenuttidm@gmail.com>
2024-03-19 20:15:12 -0300

    :wrench: :sparkles: :white_check_mark: Add components and test

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,9 @@
+import React from 'react';
 import { render, screen } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
+test('renders link with "Add New Plan" text', () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
+  const linkElement = screen.getByText(/Add New Plan/i);
   expect(linkElement).toBeInTheDocument();
 });
